ba62c7c62632b7030d6aadeddb03bd4baa7e3f20
soldiazreal <soldiazreal@gmail.com>
2022-04-13 17:12:01 -0300

    add dto

---
+++ src/main/java/model/PacienteDTO.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.util.Date;
+
+public class PacienteDTO {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private Domicilio domicilio;
+    private String documento;
+    private Date fechaAlta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Domicilio getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(Domicilio domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+}
